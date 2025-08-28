0708c9579f1c1a6c8030e99c4eacbab5666d9140
Allexndr <allexndr@users.noreply.github.com>
2025-08-28 20:07:35 +0500

    Максимально увеличен размер значка Telegram бота с w-10 h-10 на w-11 h-11 для полного заполнения кружка

--- src/components/FloatingContacts.tsx
+++ src/components/FloatingContacts.tsx
@@ -86,7 +86,7 @@ export default function FloatingContacts() {
         <img 
           src="/assets/images/telegram-bot-icon.png" 
           alt="Telegram Bot" 
-          className="w-10 h-10 object-contain"
+          className="w-11 h-11 object-contain"
         />
       ),
       title: "Telegram Bot @velta_logistics_bot",
